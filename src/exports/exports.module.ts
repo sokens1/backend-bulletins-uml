dbd773d8f93af1a18a36a82aaa4c0668cdae000c
sokens <edisokenou@gmail.com>
2026-04-21 12:20:12 +0100

    fix: resolution de l'erreur d'injection de dépendances pour ExportsService

--- src/exports/exports.module.ts
+++ src/exports/exports.module.ts
@@ -3,9 +3,10 @@ import { ExportsService } from './exports.service';
 import { ExportsController } from './exports.controller';
 import { GradesModule } from '../grades/grades.module';
 import { DatabaseModule } from '../database/database.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [GradesModule, DatabaseModule],
+  imports: [GradesModule, DatabaseModule, UsersModule],
   providers: [ExportsService],
   controllers: [ExportsController],
 })
